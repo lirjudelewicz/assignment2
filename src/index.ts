3701bfb080ea0ba18577ede74481fb34b91b56e3
Vered-Ben-David <vered054819@gmail.com>
2026-01-28 15:05:47 +0200

    init

---
+++ src/index.ts
@@ -0,0 +1,37 @@
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+dotenv.config({path: "./.env"});
+
+const app  = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+
+
+const initApp = async () => {
+    try{
+        const dbUrl = process.env.DATABASE_URL;
+        if (!dbUrl) {
+            throw new Error("DATABASE_URL is not defined");
+        }
+        await mongoose.connect(dbUrl, {});
+        const db = mongoose.connection;
+        db.on("error", (error) => {throw new Error(error)});
+        db.once("open", () => console.log("Connected to Database"));
+    }catch(err: any){
+        console.error(`Error init application: ${err.message}`);
+    }
+}
+
+initApp().then(() =>{
+    app.listen(PORT, (error) =>{
+        if(!error)
+            console.log("Server is running, listening on port "+ PORT);
+        else 
+            console.log("Error occurred, server can't start", error);
+        }
+    );
+
+})
